09e9a40908aa416cbe8ffc4a6235c85456b79bac
Scott Sickles <scottsickles@gmail.com>
2024-02-10 11:35:45 -0500

    fix the names

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "secrets" {
   value = [
     {
-      name  = "COGNITO_ACCESS_KEY_ID"
+      name  = "SES_ACCESS_KEY_ID"
       value = data.aws_secretsmanager_secret_version.access_key_id.secret_string
     },
     {
-      name  = "COGNITO_SECRET_ACCESS_KEY"
+      name  = "SES_SECRET_ACCESS_KEY"
       value = data.aws_secretsmanager_secret_version.secret_access_key.secret_string
     }
   ]
